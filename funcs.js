76fa0998650360d46e4b70ac37dd932146db224b
nasOS-official <petruhin-enot@yandex.ru>
2022-11-30 18:44:44 +0400

    1. Added function readdir() 2. Changed all var to let 3. Changed MB to MiB

--- funcs.js
+++ funcs.js
@@ -1,13 +1,13 @@
-settings_file = require("./config.json");
-var rmsb = document.querySelector(':root');
-timeclock = document.getElementById("time");
-dateclock = document.getElementById("date");
-
+let settings_file = require("./config.json");
+let rmsb = document.querySelector(':root');
+let timeclock = document.getElementById("time");
+let dateclock = document.getElementById("date");
+let mem_free;
 //=========================================Init====================================================
-var style = getComputedStyle(document.body);
+let style = getComputedStyle(document.body);
 rmsb.style.setProperty('--buttcolor', settings_file.color_btn);
-function openPage(pageName,elmnt) {
-  var i, tabcontent, tablinks;
+function openPage(pageName, elmnt) {
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -20,69 +20,81 @@ function openPage(pageName,elmnt) {
   elmnt.style.backgroundColor = style.getPropertyValue('--buttcolor');
 }
 
-function openApp(pageName,elmnt) {
-  var i, tabcontent, tablinks;
+function openApp(pageName, elmnt) {
+  let i, tabcontent, tablinks;
   document.getElementById(pageName).style.display = "block";
 
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-function memory_free(){
-  memor_free = document.getElementById("memory_free");
-  memory_used = document.getElementById("memory_used");
-  
- 
-   mem_free = parseInt(os.freemem() / 1024 / 1024);
-   memor_free.innerHTML = mem_free;
-   memory_used.innerHTML = memory - mem_free;
-   }
-   window.addEventListener('keydown', function (e) {
-      switch(e.key){
+function memory_free() {
+  let memor_free = document.getElementById("memory_free");
+  let memory_used = document.getElementById("memory_used");
+
+  mem_free = parseInt(os.freemem() / 1024 / 1024);
+  memor_free.innerHTML = mem_free;
+  memory_used.innerHTML = memory - mem_free;
+}
+window.addEventListener('keydown', function (e) {
+  switch (e.key) {
     case "Meta":
-   document.getElementById("microsoft").hidden = false;
-   break;}}, false);
-   window.addEventListener('keyup', function (e) {
-      switch(e.key){
+      document.getElementById("microsoft").hidden = false;
+      break;
+  }
+}, false);
+window.addEventListener('keyup', function (e) {
+  switch (e.key) {
     case "Meta":
-   document.getElementById("microsoft").hidden = true;
-   break;
- }}, false);
- 
+      document.getElementById("microsoft").hidden = true;
+      break;
+  }
+}, false);
+
 
 /*
 {
   "color": "#1b91ff"
 }
 */
-function shutdown(){
+function shutdown() {
   alert("Shutting down");
   // child_process.spawn("", [``])
 }
-function reboot(){
+function reboot() {
   alert("Rebooting");
   // child_process.spawn("", [``])
 }
-function hibernation(){
+function hibernation() {
   alert("Hibernating");
   // child_process.spawn("", [``])
 }
 function time() {
-  hour = new Date().getHours().toString().padStart(2, "0");
-  minutes = new Date().getMinutes().toString().padStart(2, "0");
-  seconds = new Date().getSeconds().toString().padStart(2, "0");
-  year = new Date().getFullYear()
-  month = new Date().getMonth().toString().padStart(2, "0")
-  day = new Date().getDate().toString().padStart(2, "0")
+  let hour = new Date().getHours().toString().padStart(2, "0");
+  let minutes = new Date().getMinutes().toString().padStart(2, "0");
+  let seconds = new Date().getSeconds().toString().padStart(2, "0");
+  let year = new Date().getFullYear()
+  let month = new Date().getMonth().toString().padStart(2, "0")
+  let day = new Date().getDate().toString().padStart(2, "0")
   timeclock.innerHTML = `${hour}:${minutes}:${seconds}`;
   dateclock.innerHTML = `${year}.${month}.${day}`
 }
 setInterval(time, 1000);
 time();
 
+function addapp() {
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    return console.log(file);
+  let testdirlol = fs.readdirSync("nasosapps");
+  const EXTENSION = '.json';
+
+  let targetFiles = testdirlol.filter(file => {
+    return path.extname(file).toLowerCase() === EXTENSION;
   });
-});
+  let whap = 0
+  while (whap <= targetFiles.length - 1) {
+    console.log(targetFiles[whap]);
+    console.log(require(`./nasosapps/${targetFiles[whap]}`));
+    whap++;
+  }
+}
+addapp()
--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-fs = require("fs");
+const fs = require("fs");
 const { marked } = require('marked');
-var ip = require("ip");
+const ip = require("ip");
 const { app, BrowserWindow, dialog, remote, Menu } = require('electron')
 const child_process = require('child_process');
-let os = require('os')
+const os = require('os')
 const path = require('path');
 const { executionAsyncResource } = require('async_hooks');
 const { syncBuiltinESMExports } = require("module");
 const { stringify } = require("querystring");
-user = os.userInfo().username;
-host = os.hostname();
-ipad = ip.address();
-memory = parseInt(os.totalmem() / 1024 / 1024);
+const user = os.userInfo().username;
+const host = os.hostname();
+let ipad = ip.address();
+const memory = parseInt(os.totalmem() / 1024 / 1024);
 const createWindow = () => {
   const win = new BrowserWindow({
     kiosk: true,
     webPreferences: {
-
       nodeIntegration: true,
       enableremotemodule: true,
       contextIsolation: false
@@ -24,16 +23,11 @@ const createWindow = () => {
   })
   win.loadFile('main.html');
   win.setTitle("IoT GUI");
-
 }
-
 app.whenReady().then(() => {
   createWindow()
-
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-
   })
 })
 app.on('window-all-closed', () => {
@@ -41,48 +35,42 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-
-
 function start_web() {
   child_process.spawn("xdg-open", [`https://google.com`])
-
 }
 //inode/directory
 function start_filemanager() {
   if (user != "root") {
-    child_process.spawn("xdg-open", [`/home/${user}`])
-  }
+    child_process.spawn("xdg-open", [`/home/${user}`])}
   else {
-    child_process.spawn("xdg-open", [`${user}`])
-  }
-
+    child_process.spawn("xdg-open", [`${user}`])}
 }
 function init_host() {
-  hostname = document.getElementById("host_name");
+  let hostname = document.getElementById("host_name");
   hostname.innerHTML = host;
-  usernames = document.getElementById("user_name");
+  let usernames = document.getElementById("user_name");
   usernames.innerHTML = user;
-  ip_ad = document.getElementById("ip_ad");
+  let ip_ad = document.getElementById("ip_ad");
   ip_ad.innerHTML = ipad;
-  mem = document.getElementById("memory");
+  let mem = document.getElementById("memory");
   mem.innerHTML = memory;
 }
 //=========================================Settings=code===========================================
 function settings_init() {
   let helpfile = fs.readFileSync("help.md", "utf8");
   document.getElementById('contenthelp').innerHTML = marked.parse(helpfile);
-  color = document.getElementById("setting_Color");
-  buttons = document.getElementById("app");
+  let color = document.getElementById("setting_Color");
+  let buttons = document.getElementById("app");
   console.log(color);
-  color_real = style.getPropertyValue('--buttcolor')
+  let color_real = style.getPropertyValue('--buttcolor')
   color.value = color_real;
   console.log(style.getPropertyValue('--buttcolor'));
 
   // document.styleSheets[1].insertRule('#app:active{ background-color: #fff;}', 0);
 }/*style.getPropertyValue('--buttcolor')*/
-
+  
 function save_setting() {
-  color = document.getElementById("setting_Color");
+  let color = document.getElementById("setting_Color");
   console.log(color.value);
   settings_file.color_btn = color.value;
   rmsb.style.setProperty('--buttcolor', settings_file.color_btn);
